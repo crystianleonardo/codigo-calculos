2684f26de574dce29b2f87a812d54db24c1bc18d
crys_kriss <crystianleonardo49@gmail.com>
2026-04-09 12:02:09 -0300

    Add files via upload

---
+++ calculos-crys/src/paginas/Fundamentos.jsx
@@ -0,0 +1,14 @@
+import style from './Fundamentos.module.css'
+
+export function Fundamentos(){
+    return(
+        <div className={style.conteiner}>
+            <h1>Crys</h1>
+            <div className={style.conteudo}>
+                <h2>textoooooooooooooooooo</h2>
+                <p>mais textoooooooooooooooooo</p>
+            </div>
+        </div>
+
+    )
+}
