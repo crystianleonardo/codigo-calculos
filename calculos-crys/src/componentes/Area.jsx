233b19689f9195a8825aa5e594ec035e15952c74
crys_kriss <crystianleonardo49@gmail.com>
2026-04-16 12:01:17 -0300

    Add files via upload

--- calculos-crys/src/componentes/Area.jsx
+++ calculos-crys/src/componentes/Area.jsx
@@ -1,12 +1,55 @@
 import style from './Area.module.css'
+import {useState} from 'react'
 
 export function Area(){
+    
+    const [b, setN1] = useState('')
+    const [B, setN2] = useState('')
+    const [h, setN3] = useState('')
+    const [resultado, setResultado] = useState('')
+    
+    const calcular = (e) => {
+        setResultado(((Number(b) + Number(B)) * Number(h)) / 2)
+        e.preventDefault()
+    }
+    
     return(
         <form className={style.conteiner}>
-            <div className={style.conteinerRotulo}>
-                <p className={style.rotulo}>Base</p>
-                <p className={style.rotulo}>Altura</p>
-                <p className={style.rotulo}> / 2</p>
+
+            <div className={style.conteinerCampos}>
+                <input
+                    className={style.campo}                
+                    type='number'
+                    value={b}
+                    onChange={(e) => setN1(e.target.value)}
+                    onKeyDown={(e) => e.preventDefault()}
+                />
+                <input
+                    className={style.campo} 
+                    type='number'
+                    value={B}
+                    onChange={(e) => setN2(Number(e.target.value))}
+                    onKeyDown={(e) => e.preventDefault()}               
+                />
+                <input
+                    className={style.campo} 
+                    type='number'
+                    value={h}
+                    onChange={(e) => setN3((e.target.value))}
+                    onKeyDown={(e) => e.preventDefault()}               
+                />
+
+                <p className={style.resultado}>{resultado}</p>
+            
+
+            
+            </div>
+            <div className={style.conteinerBotao}>
+                <button
+                    className={style.botao}
+                    onClick={calcular}
+                >Calcular
+                </button>
             </div>
         </form>
     )
--- calculos-crys/src/paginas/Fundamentos.jsx
+++ calculos-crys/src/paginas/Fundamentos.jsx
@@ -11,6 +11,8 @@ export function Fundamentos(){
             
             <div className={style.conteudo}>
                 <h2 className={style.subtitulo}>Área do Trapézio</h2>
+                <p className={style.texto}>Área = ((b + B) * h) / 2</p>
+                <p className={style.texto}>Um trapézio é um quadrilátero definido por possuir dois lados paralelos, conhecidos como base maior e base menor. Os outros dois lados, chamados de oblíquos, conectam as extremidades dessas bases e determinam a inclinação da figura. A distância vertical entre as bases é a altura, elemento fundamental para o cálculo de sua superfície.Para encontrar a área de um trapézio, utiliza-se a fórmula A = (B + b) · h / 2. Na prática, você deve somar as medidas da base maior (B) e da base menor (b), multiplicar esse resultado pela altura (h) e, por fim, dividir o valor total por dois. Essa fórmula permite calcular o espaço interno da figura de forma rápida e precisa para qualquer um de seus tipos, seja ele retângulo, isósceles ou escaleno.</p>
                 <Area/>
             </div>
 
